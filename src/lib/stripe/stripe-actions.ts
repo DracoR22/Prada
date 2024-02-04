5dc2b8035c57f1200c2b425e95db8984ebece4fb
DracoR22 <rdraco039@gmail.com>
2024-02-04 13:12:50 -0500

    stripe setup

---
+++ src/lib/stripe/stripe-actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+import Stripe from "stripe"
+import { db } from "../db"
+
+export const subscriptionCreated = async (subscription: Stripe.Subscription, customerId: string) => {
+   try {
+     const agency = await db.agency.findFirst({
+        where: {
+            customerId
+        },
+     })
+   } catch (error) {
+    
+   }
+}
